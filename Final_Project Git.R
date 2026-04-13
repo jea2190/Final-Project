58e3a0c4af82566f8b789a2994031db20419f2c7
Jade Amaya <jea2190@columbia.edu>
2026-04-13 15:58:08 -0400

    test

---
+++ Final_Project Git.R
@@ -0,0 +1,9 @@
+
+library("tidyverse")
+df <- read_csv("https://data.cityofnewyork.us/resource/8h9b-rp9u.csv")
+
+
+library(ggplot2)
+ggplot(df, aes(x = longitude, y = latitude)) +
+  geom_point(size = 0.5, aes(color = arrest_boro)) +
+  theme_bw()
